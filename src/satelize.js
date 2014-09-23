26418a0e19e1d2277ae63fa205a2282a03c18cb5
darul75 <darul75@gmail.com>
2014-09-23 18:36:41 +0200

    utf-8 and indexing refactoring

--- src/satelize.js
+++ src/satelize.js
@@ -5,56 +5,47 @@
  * Copyright 2014-2015 Julien Valery
  * Released under the MIT license
  */
-(function () {
 
-  var lineReader = require('line-reader');
-  var constants = require('./constants');
+var lineReader = require('line-reader');
+var constants = require('./constants');
+var Index = require('node-index');
 
-  var mg = {
-    settings: {      
-      unknown: null        
-    },
-    dicos: {}
+function SexyGender() {
+  this.settings = {      
+    unknown: null        
+  };  
+  this.dicosIdx = new Index();
+}
+
+// http://stackoverflow.com/questions/646628/how-to-check-if-a-string-startswith-another-string
+if (typeof String.prototype.startsWith != 'function') {
+  // see below for better implementation!
+  String.prototype.startsWith = function (str){
+    return this.indexOf(str) == 0;
   };
+}
 
-  // http://stackoverflow.com/questions/646628/how-to-check-if-a-string-startswith-another-string
-  if (typeof String.prototype.startsWith != 'function') {
-    // see below for better implementation!
-    String.prototype.startsWith = function (str){
-      return this.indexOf(str) == 0;
-    };
-  }
-
-  if (typeof String.prototype.isNotEmpty != 'function') {
-    String.prototype.isNotEmpty = function(element) {
-      return element.trim() != '';
-    };
-  }
-    
-  //// exported mg module functions ////
-
-  mg.getGender = function (params, cb) {
-    if (!params.string)
-      return;    
-
-    var name = params.string.toLowerCase();
-
-    var result = {};
-
-    var idx = constants.COUNTRIES.indexOf(params.country);
-
-    var found = this.dicos[name];
-
-
-    if (country) {
+if (typeof String.prototype.isNotEmpty != 'function') {
+  String.prototype.isNotEmpty = function(element) {
+    return element.trim() != '';
+  };
+}
       
 
-    }
-    else {
+SexyGender.prototype.getGender = function (params, cb) {
+  if (!params.string)
+    return;    
 
+  var name = params.string.toLowerCase();
 
-    }
+  var result = {};
 
+  var idx = constants.COUNTRIES.indexOf(params.country);
+
+  var found = this.dicos[name];
+
+  console.log(found);
+  
     /*cb = cb || function () {};
     if (!arr.length) {
         return callback();
@@ -75,40 +66,44 @@
           }
       }
     }*/
-  };
+};
 
-  mg.nameExists = function(name) {        
-    return this.dicos.hasOwnProperty(name);
-  };  
+SexyGender.prototype.nameExists = function(name) {        
+  return this.dicos.hasOwnProperty(name);
+};
 
-  var _readDico = function (cb) {    
-    lineReader.eachLine(constants.DIC_FILE, function(line, last) {
-      _digestLine.apply(null, arguments);
-    }).then(function () {
-      mg.nameExists('Julien');
-      mg.getGender({string: 'Julien'});
-    });
-  };  
+var _readDico = function (cb) {    
+  lineReader.eachLine(constants.DIC_FILE, function(line, last) {
+    _digestLine.apply(sg, arguments);
+  }).then(function () {
+    sg.nameExists('Julien');
+    sg.getGender({string: 'Julien'});
+    sg.getGender({string: 'Mateušas'});
+    
+  });
+};  
 
-  var _digestLine = function(line, last) {
-    if (line.startsWith('#'))
-      return;    
+var _digestLine = function(line, last) {
+  if (line.startsWith('#'))
+    return;    
 
-    var parts = line.split(' ').filter(''.isNotEmpty);
-    var name = parts[1].toLowerCase();
+  var parts = line.split(' ').filter(''.isNotEmpty);
+  var name = parts[1].toLowerCase();  
 
-    mg.dicos[name] = {
-      name: name,
-      gender: parts[0],
-      countries: line.slice(30, line.length)
-    };
-        
-  };
+  this.dicosIdx.addDocument(name, {
+    name: name,
+    gender: parts[0],
+    countries: line.slice(30, line.length)
+  });
+      
+};
 
-  _readDico(function() {
-    name_exists
-  });    
+/**
+* Export default singleton.
+*/
+var sg = new SexyGender();
+module.exports = sg;
 
-  module.exports = mg;    
-
-}());
+_readDico(function() {
+  name_exists
+});
